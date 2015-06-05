7db40b7821622e215dafa8f785b2d94f2c6de9e1
jisenhour <jisenhour@commonapp.org>
2015-06-05 13:47:40 -0400

    New xml utilities file

---
+++ xmlutils.ps1
@@ -0,0 +1,13 @@
+﻿function Format-XML ([xml]$xml, $indent=2) 
+{ 
+    $StringWriter = New-Object System.IO.StringWriter 
+    $XmlWriter = New-Object System.XMl.XmlTextWriter $StringWriter 
+    $xmlWriter.Formatting = "indented" 
+    $xmlWriter.Indentation = $Indent 
+    $xml.WriteContentTo($XmlWriter) 
+    $XmlWriter.Flush() 
+    $StringWriter.Flush() 
+    Write-Output $StringWriter.ToString() 
+}
+
+ Format-XML ([xml](cat C:\xmltest\web.config)) -indent 4 > C:\xmltest\test.config
