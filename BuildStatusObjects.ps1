b70a9006e9596fb2a8e9b2e8779e1754c67d5e1f
Jeff Isenhour <jisenhour@commonapp.org>
2015-09-10 09:43:56 -0400

    Delete BuildStatusObjects.ps1

--- BuildStatusObjects.ps1
+++
@@ -1,9 +0,0 @@
-﻿$URI = "http://10.2.31.135/DataCatalogService.svc?wsdl"
-$WebServiceObject = New-WebServiceProxy -Uri $URI 
-$ScreenStatusArray = Get-Content C:\DefScreenStatus\ScreenStatusValuesLT.txt
-foreach ($ScreenStatus in $ScreenStatusArray) 
-{
-    $WebServiceObject.BuildDefaultScreenStatus($ScreenStatus) 
-    $ScreenStatus
-
-}
